abc0794cc8d42864d5b65d12ddfcc0d2d5764401
DarkKavon <kacperwandel@gmail.com>
2022-06-07 22:12:42 +0200

    Clean up

--- app/src/main/java/com/example/am_lecture/ActivityActivity.kt
+++ app/src/main/java/com/example/am_lecture/ActivityActivity.kt
@@ -1,5 +1,6 @@
 package com.example.am_lecture
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
@@ -12,7 +13,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -25,13 +25,14 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import org.json.JSONObject
+import com.maps.route.extensions.drawRouteOnMap
 
 class ActivityActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private var weather_api_key = "d179ae0e16fe4cceb2130cb51520c687"
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var locationManager: LocationManager
-    lateinit var dbhelper: DBHelper
+    private lateinit var dbhelper: DBHelper
     private lateinit var shared: SharedPreferences
     private var username: String = ""
     private lateinit var mapFragment: SupportMapFragment
@@ -44,6 +45,8 @@ class ActivityActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var currMarkerOptions: MarkerOptions
     private lateinit var currMarker: Marker
     private var zoomLevel: Float = 15.0f //This goes up to 21
+    private lateinit var oldLoc : LatLng
+    private lateinit var newLoc :LatLng
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,11 +59,11 @@ class ActivityActivity : AppCompatActivity(), OnMapReadyCallback {
 
         mapFragment =
             (supportFragmentManager.findFragmentById(R.id.map_fragment) as? SupportMapFragment)!!
-        mapFragment?.getMapAsync(this)
+        mapFragment.getMapAsync(this)
 
-        val logout_btn = findViewById(R.id.logout_btn) as Button
-        weather_text = findViewById(R.id.weather) as TextView
-        logout_btn.setOnClickListener() {
+        val logout_btn = findViewById<Button>(R.id.logout_btn)
+        weather_text = findViewById(R.id.weather)
+        logout_btn.setOnClickListener {
             username = ""
             setUser()
             Toast.makeText(applicationContext, "Logout successful!", Toast.LENGTH_SHORT).show()
@@ -112,6 +115,7 @@ class ActivityActivity : AppCompatActivity(), OnMapReadyCallback {
         )
     }
 
+    @SuppressLint("SetTextI18n")
     private val locationListener =
         android.location.LocationListener { location -> //handle location change
             println(location)
@@ -119,58 +123,52 @@ class ActivityActivity : AppCompatActivity(), OnMapReadyCallback {
             long = location.longitude
             updateMap(mMap)
             getTemp()
-            if (temp == "" || city == "") {
-                weather_text.text = "Click update weather"
-            } else {
-                weather_text.text = "Now it's " + temp + "°C in " + city
+            if (temp != "" && city != "") {
+                weather_text.text = "Now it's $temp°C in $city"
             }
         }
 
-    fun getTemp() {
+    private fun getTemp() {
         val queue = Volley.newRequestQueue(this)
-        val url: String =
-            "https://api.weatherbit.io/v2.0/current?" + "lat=" + latt + "&lon=" + long + "&key=" + weather_api_key
-        println("lat " + url)
+        val url =
+            "https://api.weatherbit.io/v2.0/current?lat=$latt&lon=$long&key=$weather_api_key"
+        println("lat $url")
 
         val stringReq = StringRequest(
             Request.Method.GET, url,
-            Response.Listener<String> { response ->
-//                println("lat " + response.toString())
-
+            { response ->
                 val obj = JSONObject(response)
-
                 val arr = obj.getJSONArray("data")
-//                println("lat obj1 " + arr.toString())
-
                 val obj2 = arr.getJSONObject(0)
-//                println("lat obj2 " + obj2.toString())
-
-                println(obj2.getString("temp") + " deg Celsius in " + obj2.getString("city_name"))
+//                println(obj2.getString("temp") + " deg Celsius in " + obj2.getString("city_name"))
                 temp = obj2.getString("temp")
                 city = obj2.getString("city_name")
             },
-            // In case of any error
-            Response.ErrorListener { println("That didn't work!") })
+            { println("That didn't work!") })
         queue.add(stringReq)
     }
 
     override fun onMapReady(p0: GoogleMap) {
         mMap = p0
-        val pos = LatLng(latt, long)
-        currMarkerOptions = MarkerOptions().position(pos).title("Marker in " + city)
+        oldLoc = LatLng(latt, long)
+        currMarkerOptions = MarkerOptions().position(oldLoc).title("Marker in $city")
         currMarker = p0.addMarker(currMarkerOptions)!!
-//        p0.moveCamera(CameraUpdateFactory.newLatLng(pos))
-        p0.moveCamera(CameraUpdateFactory.newLatLngZoom(pos, zoomLevel));
+        p0.moveCamera(CameraUpdateFactory.newLatLngZoom(oldLoc, zoomLevel))
     }
 
     private fun updateMap(p0: GoogleMap) {
-        val pos = LatLng(latt, long)
+        newLoc = LatLng(latt, long)
         currMarker.remove()
-        currMarkerOptions = MarkerOptions().position(pos).title("Marker in " + city)
+        currMarkerOptions = MarkerOptions().position(newLoc).title("Marker in $city")
         currMarker = p0.addMarker(currMarkerOptions)!!
-//        p0.moveCamera(CameraUpdateFactory.newLatLng(pos))
-        p0.moveCamera(CameraUpdateFactory.newLatLngZoom(pos, zoomLevel));
+        p0.moveCamera(CameraUpdateFactory.newLatLngZoom(newLoc, zoomLevel))
+
+        //DRAW HERE
+        
+
+        oldLoc = newLoc
     }
+
 }
 
 
